4a834e0275a774a5f724608d34ff51b2d302829b
Jasmeet-1998 <devs.us.1984@gmail.com>
2022-07-09 13:34:57 +0530

    rxjs: mergeMap, exhaustMap
    covers
    - interval()
    - mergeMap - no sequential execution of observables
    - exhaustMap- for situation where we want first come first serve processing of observable processing in case of multiple observable streams

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-> 🎁 collection of miscellaneous topics with snippets and explainations
+> # 🎁 collection of miscellaneous topics with snippets and explainations
 
-- this repo contains snippets, walkthorugh, study content for intermediate & advance topics in different domains.
+**this repo contains snippets, walkthorugh, study content for intermediate & advance topics in different domains.**
 
-1.  RXJS higher order mapping operators (use in observable env)
+> ## 1. RXJS higher order mapping operators (use in observable env)
+
     [ref- https://www.learnrxjs.io/]
 
         #to trans-compile ts-->js
@@ -34,3 +35,15 @@
 - it is going to handle each value in observable stream sequentially i.e in the order they were emitted initially
 
 - **it will not move to the next value in observable stream until it has completed the current value processing. like if [2000,1000] , so first it will process 2000 then move to 1000.**
+
+> mergeMap (not-sequential i.e unlike concatMap)
+
+- maps each value that an observable emits similar to map and concatMap.
+
+- **the difference in mergeMap is that all the values in th observable stream gets passed to the pipe() at the same time, example [2000,1000] on using with mergeMap will print out 1000, 2000 i.e reverse order because all the values were provided to the pipe at the same time in mergeMap**
+
+> 📝:IMP exhaustMap (only concerned about current processing observable while other observables are ignored)
+
+- **ref: index.ts**
+
+- **used in situation where we need to merge two observable streams and we need to just have concern on the observable stream processing that gets executed first while the other streams get ignored completely.**
--- rxjs_higher_orderMapping_operators/index.ts
+++ rxjs_higher_orderMapping_operators/index.ts
@@ -12,32 +12,58 @@ import  {
 // 📝: an observable stream that emits 2000 then 1000 values
 let source = of(2000,1000);
 
-console.log('\n[BASIC]  a  observable stream of numbers one after another in the order they were emitted via of():');
-// 📝: subscribe function can be used on this observable stream to get these values
-source.subscribe((value)=>{
-    console.log(value)
-});
+// console.log('\n[BASIC]  a  observable stream of numbers one after another in the order they were emitted via of():');
+// // 📝: subscribe function can be used on this observable stream to get these values
+// source.subscribe((value)=>{
+//     console.log(value)
+// });
 
-console.log('\n[MAP] applying transforms(add 100) via map&pipe in rxjs to each value observable stream: ');
-// basics- map in rxjs, transform values of the observable stream
-source = source.pipe(
-    map((value)=>{
-        return value+100;
-    })
-);
+// console.log('\n[MAP] applying transforms(add 100) via map&pipe in rxjs to each value observable stream: ');
+// // basics- map in rxjs, transform values of the observable stream
+// source = source.pipe(
+//     map((value)=>{
+//         return value+100;
+//     })
+// );
 
 
-source.subscribe((value)=>{
-    console.log(value)
-});
+// source.subscribe((value)=>{
+//     console.log(value)
+// });
 
-console.log('\n [concatMap]');
-// 1. concatMap
-const concatMapExample = source.pipe(
-    // 📝: the inner "of" observable is flattened as the value emit from this of() is the final value emitted from concatMap rxjs operators
-    concatMap(value=> of(`Delayed by ${value}ms`).pipe(delay(value)))
-);
+// console.log('\n [concatMap]');
+// // 1. concatMap
+// const concatMapExample = source.pipe(
+//     // 📝: the inner "of" observable is flattened as the value emit from this of() is the final value emitted from concatMap rxjs operators
+//     concatMap(value=> of(`Delayed by ${value}ms`).pipe(delay(value)))
+// );
 
-concatMapExample.subscribe((value)=>{
-    console.log(value)
-});
+// concatMapExample.subscribe((value)=>{
+//     console.log(value)
+// });
+
+// console.log('\n [mergeMap]');
+// // 2. mergeMap
+// const mergeMapExample = source.pipe(
+//     mergeMap(value=> of(`Delayed by ${value}ms`).pipe(delay(value)))
+// );
+
+// mergeMapExample.subscribe((value)=>{
+//     console.log(value)
+// });
+
+// 📝: an observable via interval() that emits number every 1000 ms i.e 1 sec
+const sourceInterval = interval(1000);
+// 📝: a second observable , delaying with only 10 ms and take only first four number emmited by sourceInterval observable
+const delayedInterval = sourceInterval.pipe(delay(10),take(4));
+
+console.log('\n [exhaustMap]');
+// 3. exhaustMap
+
+// 📝: rxjs merge() is used to merge different observable streams so that they run at same time
+merge(
+    delayedInterval, // delayed observable that gets completely ignored by exhaustMap
+    of(true) // instant observable that returns true , hence we get 5 values not 4 values
+).pipe(exhaustMap(_ => sourceInterval.pipe(take(5)))).subscribe(val=>{
+    console.log(val)
+})
